c449a4f52a74580b588c85bcd63526f37c808f10
Blackchinese <daniel.surya@si.ukdw.ac.id>
2019-09-04 21:58:21 +0700

    Tugas Kelas

--- app/src/main/java/ukdw/com/proteintracker/Main2Activity.java
+++ app/src/main/java/ukdw/com/proteintracker/Main2Activity.java
@@ -3,6 +3,7 @@ package ukdw.com.proteintracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+
+        TextView myTextView = new TextView(this);
+        myTextView.setText("hehe");
+
+        Bundle b = getIntent().getExtras();
+        String helpText = b.getString("helpString");
+        myTextView.setText(helpText);
+        setContentView(myTextView);
     }
 }
--- app/src/main/java/ukdw/com/proteintracker/MainActivity.java
+++ app/src/main/java/ukdw/com/proteintracker/MainActivity.java
@@ -1,6 +1,5 @@
 package ukdw.com.proteintracker;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,58 +17,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView textView = (TextView) findViewById(R.id.mainActivityTextView);
-        textView.setText("text untuk update view");
+        textView.setText(R.string.test_untuk_update_view);
+        Button mylay = findViewById(R.id.btn_layout);
+        mylay.setOnClickListener(btnLayout);
 
-        Button helpBtn = (Button) findViewById(R.id.helpButton);
-        helpBtn.setOnClickListener((helpButtonListener));
-
-        Button btnLayout = (Button) findViewById((R.id.btnLayout));
-        btnLayout.setOnClickListener(new View.OnClickListener(){
+        Button myBtn = (Button) findViewById(R.id.button1);
+        myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Intent i = new Intent (MainActivity.this,Main2Activity.class);
-                startActivity(i);
+            public void onClick(View view) {
+                EditText myEditText = (EditText) findViewById(R.id.editText1);
+                Log.d("Proteintracker", myEditText.getText().toString());
             }
         });
 
-        if(savedInstanceState != null)
-
-
-        textView.setText(R.string.test_untuk_update_view);
-        Button helpButton = (Button)findViewById(R.id.helpButton);
-        helpBtn.setOnClickListener(helpButtonListener);
-
-        if(savedInstanceState != null){
-            Log.d("ProteinTracker",savedInstanceState.getString("abc"));
-        }
-
-        Button myBtn = (Button)findViewById(R.id.button1);
+        Button myBtn1 = (Button) findViewById(R.id.button1);
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { EditText myEditText = (EditText)findViewById(R.id.editText1);
-            Log.d("Proteintracker",myEditText.getText().toString());
+            public void onClick(View view) {
+                EditText myEditText = (EditText) findViewById(R.id.editText1);
+                Log.d("Proteintracker", myEditText.getText().toString());
             }
-          }
-        );
-    }
+        });
 
-    private View.OnClickListener helpButtonListener = new View.OnClickListener() {
+        Button helpBtn = (Button)findViewById(R.id.helpButton);
+        helpBtn.setOnClickListener(helpButtonListener);
+
+
+        if(savedInstanceState != null){
+            Log.d("ProteinTracker",savedInstanceState.getString("abc")); }
+    }
+    private View.OnClickListener helpButtonListener = new View.OnClickListener()
+    {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this,HelpActivity.class);
-            Bundle b = new Bundle(); EditText myEditText = (EditText)findViewById(R.id.editText1);
+            Bundle b = new Bundle();
+
+            EditText myEditText = (EditText)findViewById(R.id.editText1);
+
             b.putString("helpString",myEditText.getText().toString());
             intent.putExtras(b);
+
             startActivity(intent);
         }
     };
-
-    private View.OnClickListener myBtnClickListener = new View.OnClickListener() {
+    private  View.OnClickListener btnLayout = new View.OnClickListener(){
         @Override
-        public void onClick(View view) {
-            EditText myEditText = (EditText) findViewById(R.id.editText1);
-            Log.d("Proteintracker", myEditText.getText().toString());
+        public void onClick(View view){
+            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(intent);
         }
     };
 
@@ -81,5 +79,3 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 }
-
-
