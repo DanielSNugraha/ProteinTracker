fdc4c4594afa6fceab6509339f8066c6610c9ead
Blackchinese <daniel.surya@si.ukdw.ac.id>
2019-09-11 19:09:20 +0700

    11 Sept 19

---
+++ app/src/main/java/ukdw/com/proteintracker/fragmentActivity.java
@@ -0,0 +1,34 @@
+package ukdw.com.proteintracker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class fragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        Button btnPress = findViewById(R.id.buttonTesFragment);
+        btnPress.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ProteinFragment proteinFragment = ProteinFragment.newInstance("Mulut anda kotor !","Bangtat Kau");
+                ft.replace(R.id.frameMain, proteinFragment);
+                ft.commit();
+
+             }
+            }
+        );
+    }
+}
+
+
