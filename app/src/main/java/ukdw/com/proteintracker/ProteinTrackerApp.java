66713d555f9168ff2a14ee2862e691034ad249ce
Blackchinese <daniel.surya@si.ukdw.ac.id>
2019-11-06 19:05:59 +0700

    Tugas EZ

--- app/src/main/java/ukdw/com/proteintracker/ProteinTrackerApp.java
+++ app/src/main/java/ukdw/com/proteintracker/ProteinTrackerApp.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,21 +16,35 @@ public class ProteinTrackerApp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = ProteinTrackerApp.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+        String statusLogin = prefs.getString("isLogin",null); Button btnLogin = (Button)findViewById(R.id.btnLogim);
+        btnLogin.setOnClickListener(myBtnLoginClick);
+        if (statusLogin != null){ btnLogin.setText("Logout");
+        }
+        else{ btnLogin.setText("Login");
+        }
+
+        setContentView(R.layout.activity_main);
+        Button btnSetting = (Button)findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(myBtnSettingClick);
+
+
         setContentView(R.layout.activity_proteintrackerapp);
 
         Button resetButton = (Button)findViewById(R.id.btnReset);
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(ProteinTrackerApp.this);
-                builder.setMessage("Apakah anda yakin untuk mereset nilai protein?")
+                resetButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ProteinTrackerApp.this);
+                        builder.setMessage("Apakah anda yakin untuk mereset nilai protein?")
 
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(ProteinTrackerApp.this, "Tidak jadi reset", Toast.LENGTH_SHORT).show();
-                            } })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(ProteinTrackerApp.this, "Tidak jadi reset", Toast.LENGTH_SHORT).show();
+                                    } })
 
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 Toast.makeText(ProteinTrackerApp.this, "Melakukan RESET !!", Toast.LENGTH_SHORT).show();
                             } });
@@ -36,4 +52,22 @@ public class ProteinTrackerApp extends AppCompatActivity {
             }
         });
     }
+    private View.OnClickListener myBtnSettingClick = new View.OnClickListener() {
+        @Override public void onClick(View v) {
+            Intent intent = new Intent(ProteinTrackerApp.this, SettingsProteinTracker.class);
+            startActivity(intent);
+        };
+    };
+
+    private View.OnClickListener myBtnLoginClick = new View.OnClickListener() {
+        @Override public void onClick(View v) {
+            SharedPreferences prefs = ProteinTrackerApp.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+            String statusLogin = prefs.getString("isLogin",null); SharedPreferences.Editor edit = prefs.edit();
+            Button btnLogin = (Button)findViewById(R.id.btnLogim); if (statusLogin != null){ edit.putString("isLogin",null);
+            btnLogin.setText("Login"); }else{ edit.putString("isLogin","Admin"); btnLogin.setText("Logout");
+            }
+            edit.commit();
+        }
+    };
 }
+
